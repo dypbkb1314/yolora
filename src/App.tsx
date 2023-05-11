0f427ddbac9df149bf79281c5c4a5e10fb934624
dyp <yuanpeng.diao@vcg.com>
2023-05-11 17:57:18 +0800

    feat: add eslint/prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <span>hello jenkins</span>
+      <span>add eslint/prettier</span>
     </div>
   );
 }
